083819a11dc8cd24710de6df9cf2e5226036dd9d
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-19 20:23:31 +0900

    [Update]9

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top,:about]
+  add_flash_types :success, :info, :warning, :danger
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def after_sign_in_parh_for(resource)
+     user_path(current_user)
+  end
+
+  def after_sign_up_path_for(resource)
+   user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+end
